d2f71a57407e22039a2887d135e6add26b4ee152
SeHyun11103 <dotori506@gmail.com>
2023-10-19 15:19:57 +0900

    1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int flag=1;
+  int y=3; 
+  while (flag!=0)
+  {
+        flag=0;
+        }
+  y=4;
+  
+
+  system("PAUSE");	
+  return 0;
+}
